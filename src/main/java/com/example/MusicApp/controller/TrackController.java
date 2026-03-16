26db89d5cc7cac6b8207426214b45a40f5c8a670
Đỗ An Bình <binhdath06811@gmail.com>
2026-03-16 21:59:34 +0700

    remove index page

--- src/main/java/com/example/MusicApp/controller/TrackController.java
+++ src/main/java/com/example/MusicApp/controller/TrackController.java
@@ -7,7 +7,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
