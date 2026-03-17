79be3e617703032df895a5f4bb8c220db568a7c0
Đỗ An Bình <binhdath06811@gmail.com>
2026-03-17 14:39:16 +0700

    check

---
+++ src/main/java/com/example/MusicApp/Logic.java
@@ -0,0 +1,24 @@
+package com.example.MusicApp;
+
+import java.util.Random;
+
+public class Logic {
+    public static void shuffleArray(int[] array) {
+        Random random = new Random();
+
+        // Start from the last element and swap one by one
+        for (int i = array.length - 1; i > 0; i--) {
+
+            // Pick a random index from 0 to i
+            int j = random.nextInt(i + 1);
+
+            // Swap array[i] with the element at random index array[j]
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+
+//    public void shuffleByGenres()
+}
